65dfb071bfb434ba61d377f7db432e839c9b2b5d
unknown <ltv400@yandex.ru>
2020-01-10 05:57:17 +0400

    initial assembly

---
+++ nuxt.config.js
@@ -0,0 +1,88 @@
+
+module.exports = {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    {
+      src: '~/assets/styles/styles.scss',
+      lang: 'scss'
+    },
+    {
+      src: '~/assets/styles/reset.scss',
+      lang: 'scss'
+    }
+  ],
+  styleResources: {
+    scss: './assets/styles/settings.scss'
+  },
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    // Doc: https://github.com/nuxt-community/stylelint-module
+    '@nuxtjs/stylelint-module'
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extractCSS: process.env.NODE_ENV === 'production',
+    extend (config, ctx) {
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    },
+    postcss: {
+      preset: {
+        autoprefixer: {
+          flexbox: true,
+          grid: true
+        }
+      }
+    }
+  },
+  server: {
+    host: '0.0.0.0',
+    port: '3000'
+  }
+}
